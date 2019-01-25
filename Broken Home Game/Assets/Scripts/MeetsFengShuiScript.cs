e56292e4ee31dfb9c6fbaac5ce8a4b2ea30c5900
James Picone <jamesmpicone@gmail.com>
2019-01-25 21:39:52 +1030

    some scripts

---
+++ Broken Home Game/Assets/Scripts/MeetsFengShuiScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class MeetsFengShuiScript : MonoBehaviour
+{
+    FurnitureRule[] rules;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rules = GetComponentsInChildren<FurnitureRule>();
+    }
+
+    public bool HasFengShui()
+    {
+        return rules.All(f => f.Passes());
+    }
+}

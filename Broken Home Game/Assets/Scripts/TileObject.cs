46551be26d6fa0c395b88baf327a8a5b70143c66
James Picone <jamesmpicone@gmail.com>
2019-01-25 23:19:30 +1030

    Add statics

---
+++ Broken Home Game/Assets/Scripts/TileObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileObject : MonoBehaviour
+{
+
+    Vector2Int cell;
+
+    public Vector2Int GetCell() { return cell; }
+
+    void MoveToCell(Tilemap tilemap, Vector2Int newCell)
+    {
+        transform.parent = tilemap.transform;
+        transform.position = tilemap.CellToLocal(new Vector3Int(newCell.x, newCell.y, 0));
+        cell = newCell;
+    }
+
+
+}
---
+++ Broken Home Game/Assets/Scripts/statics/Types.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Rotation
+{
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
+    ASKEW
+}
+
+static class ExtensionMethods
+{
+    public static Vector2Int Step(this Vector2Int pos, Rotation rotation)
+    {
+        switch (rotation)
+        {
+            case Rotation.NORTH: ++pos.y; break;
+            case Rotation.EAST: ++pos.x; break;
+            case Rotation.SOUTH: --pos.y; break;
+            case Rotation.WEST: --pos.x; break;
+            default: break;
+        }
+
+        return pos;
+    }
+}

58e565e43d49666ad428d87b0bdfa291b7d11fda
James Picone <jamesmpicone@gmail.com>
2019-01-25 22:24:42 +1030

    Furniture

---
+++ Broken Home Game/Assets/Scripts/InteractableFurnitureScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class InteractableFurnitureScript : MonoBehaviour
+{
+    void Place(Tilemap map, Vector3Int cell)
+    {
+        gameObject.transform.parent = map;
+        gameObject.transform.localPosition = map.CellToLocal(cell);
+
+        var zoneScript = gameObject.GetComponentInParent<ZoneScript>();
+        zoneScript.OnUpdateLayout();
+    }
+
+    void Pickup(PlayerMovement player)
+    {
+        var zoneScript = gameObject.GetComponentInParent<ZoneScript>();
+        
+        gameObject.transform.parent = player.gameObject;
+        gameObject.transform.localPosition = new Vector3(0, 0, 1);
+        gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+        zoneScript.OnUpdateLayout();
+    }
+
+    void Rotate()
+    {
+        gameObject.transform.localRotation *= Quaternion.AngleAxis(90, new Vector3(0, 0, 1));
+    }
+}

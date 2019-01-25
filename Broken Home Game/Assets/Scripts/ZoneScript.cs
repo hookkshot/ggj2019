95de89baf7b02d1ae7e20f630b3361bc9bd1124f
James Picone <jamesmpicone@gmail.com>
2019-01-25 22:12:20 +1030

    Zone scripot

---
+++ Broken Home Game/Assets/Scripts/ZoneScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneScript : MonoBehaviour
+{
+    List<MeetsFengShuiScript> furniture;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        furniture = new List<MeetsFengShuiScript>(GetComponentsInChildren<MeetsFengShuiScript>());
+    }
+
+    public void OnUpdateLayout() {
+        foreach (var script in furniture)
+        {
+            script.UpdateFengShui();
+        }
+    }
+}

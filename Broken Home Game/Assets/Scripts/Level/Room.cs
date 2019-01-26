4df71e6d2c94ebd6e0630e08f83362deb413db88
Keirron Stach <stachkeirron@gmail.com>
2019-01-26 17:22:11 +1030

    Updated room.

--- Broken Home Game/Assets/Scripts/Level/Room.cs
+++ Broken Home Game/Assets/Scripts/Level/Room.cs
@@ -1,18 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Tilemaps;
 
 public class Room : MonoBehaviour
 {
+    public string SceneName;
+
+    [SerializeField]
+    private LevelDoor[] doors;
+
+    public Tilemap Tilemap;
+    private ZoneScript[] zones;
+    private PlayerController playerController;
+    private PlayerInventory playerInventory;
+
+    public void Setup(PlayerController playerController)
+    {
+        zones = GetComponentsInChildren<ZoneScript>();
+        this.playerController = playerController;
+        playerInventory = playerController.GetComponent<PlayerInventory>();
+    }
+
+    public LevelDoor GetDoor(string name)
+    {
+        return doors.FirstOrDefault(d => d.Name == name);
+    }
+
     public void OnZoneUpdate()
     {
         bool hasFengShui = true;
 
-        var zones = GetComponentsInChildren<ZoneScript>();
-        foreach (var zone in zones) {
+        foreach (var zone in zones)
+        {
             hasFengShui &= zone.HasFengShui;
         }
 
-        var player = GetComponentInChildren<PlayerInventory>();
-        hasFengShui &= player.IsInventoryEmpty();
+        hasFengShui &= playerInventory.IsInventoryEmpty();
     }
+
+
 }

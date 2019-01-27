1a883cb5400c87e82bfb76fe33c30fd42b9b3a1c
Simon Milne <simon@limerocket.com>
2019-01-27 12:31:49 +1030

    Must Face south rule

--- Broken Home Game/Assets/Scripts/Rules/MustFaceEastWest.cs
+++ Broken Home Game/Assets/Scripts/Rules/MustFaceEastWest.cs
@@ -5,11 +5,6 @@ public class MustFaceEastWest : FurnitureRule
 {
     public override bool Passes(Furniture checkingObject)
     {
-        if (checkingObject.TileObject.GetRotation() == Rotation.EAST || checkingObject.TileObject.GetRotation() == Rotation.WEST)
-        {
-            return true;
-        }
-
-        return false;
+        return (checkingObject.TileObject.GetRotation() == Rotation.EAST || checkingObject.TileObject.GetRotation() == Rotation.WEST);
     }
 }

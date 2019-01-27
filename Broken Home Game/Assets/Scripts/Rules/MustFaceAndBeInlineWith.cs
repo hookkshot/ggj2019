e2eb83e6800b0f005aca42483d5179eb70267607
James Picone <jamesmpicone@gmail.com>
2019-01-27 14:48:16 +1030

    Entertainment unit

---
+++ Broken Home Game/Assets/Scripts/Rules/MustFaceAndBeInlineWith.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Furniture Rules/MustFaceAndBeInlineWith")]
+public class MustFaceAndBeInlineWith : FurnitureRule
+{
+    [SerializeField] private string[] tagsToCheck = null;
+
+    public override bool Passes(Furniture checkingObject)
+    {
+        List<Furniture> furnitureInZone = checkingObject.Zone.Furniture;
+
+        foreach (Furniture furniture in furnitureInZone)
+        {
+            if (furniture == checkingObject)
+            {
+                continue;
+            }
+
+            if (furniture.HasAnyTags(tagsToCheck))
+            {
+                if (IsFacingObject(furniture, checkingObject))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public bool IsFacingObject(Furniture target, Furniture furniture)
+    {
+        if (target.TileObject.GetRotation() == furniture.TileObject.GetRotation())
+        {
+            return false;
+        }
+
+        if (target.TileObject.Cell.x == furniture.TileObject.Cell.x || target.TileObject.Cell.y == furniture.TileObject.Cell.y)
+        {
+            int oppositeDirection = ((int)target.TileObject.GetRotation() + 2) % 4;
+            if (oppositeDirection == (int)furniture.TileObject.GetRotation())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
